a367b21181de653e12697c064647aa0ca2a25b82
hassan8118 <mujtabahassan1234567@gmail.com>
2023-04-20 11:00:56 -0400

    CarsDone

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Audi";
+            myCar.Model = "R8";
+            myCar.Year = 2020;
+
+            //var carList = new List<car>();
+            Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year}");
+
         }
     }
 }
